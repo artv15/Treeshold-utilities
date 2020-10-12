19448f9b9602d3d798902bff28a4a7808bbb158c
artv15 <artv15@users.noreply.github.com>
2020-10-12 16:46:57 +0300

    Update voice.py

--- cogs/voice.py
+++ cogs/voice.py
@@ -68,7 +68,6 @@ class voice(commands.Cog):
                     print("BotInfo = " + str(self.bot.user))
                     await member.move_to(channel2)
                     await channel2.set_permissions(self.bot.user, connect=True,read_messages=True)
-                    #await channel2.set_permissions(giverights, manage_channel=True)
                     await channel2.edit(name= name, user_limit = limit)
                     c.execute("INSERT INTO voiceChannel VALUES (?, ?)", (id,channelID))
                     conn.commit()
