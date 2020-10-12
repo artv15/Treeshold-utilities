cf862685c4684115546fec246d6be8d90babe9dc
artv15 <artv15@users.noreply.github.com>
2020-10-12 13:07:46 +0300

    Test

--- TreesHoldUtilities.py
+++ TreesHoldUtilities.py
@@ -33,7 +33,7 @@ import sys
 client = discord.Client()
 
 bot = commands.Bot(command_prefix=".")
-
+bot.remove_command("help")
 initial_extensions = ['cogs.voice']
 
 if __name__ == '__main__':
@@ -43,6 +43,14 @@ if __name__ == '__main__':
         except Exception as e:
             print(f'Failed to load extension {extension}.', file=sys.stderr)
             traceback.print_exc()
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
 
 ban_message_id = 0
 def buildblock(size):
