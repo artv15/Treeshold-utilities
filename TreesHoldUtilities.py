cb03b856b4cfb53a582ecdfdbdbbeedcff1f49a0
artv15 <artv15@users.noreply.github.com>
2020-10-12 07:15:10 +0300

    Ааааааа

--- TreesHoldUtilities.py
+++ TreesHoldUtilities.py
@@ -202,9 +202,9 @@ async def source(ctx):
 @Bot.command(pass_context=True)
 @commands.has_permissions(administrator=True)
 @commands.has_role("Shout permission")
+author = ctx.author
 async def shoutout(ctx, *, content: str):
 	emb = discord.Embed(title="Громкое заявление!", description=content)
-	author = ctx.author
 	emb.add_field(name="Заявление сделано:", value=str(author))
 	await ctx.send(embed=emb)
 
