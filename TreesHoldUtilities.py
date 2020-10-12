8a1c11d289d9dbe60d1a8d5bcae5a1d516e11d28
artv15 <artv15@users.noreply.github.com>
2020-10-12 13:38:01 +0300

    Update TreesHoldUtilities.py

--- TreesHoldUtilities.py
+++ TreesHoldUtilities.py
@@ -318,7 +318,7 @@ voice = Bot
 #    def __init__(self, bot):
 #        self.bot = bot
     
-@commands.Cog.listener()
+@commands.listener()
 async def on_voice_state_update(self, member, before, after):
     conn = sqlite3.connect('voice.db')
     c = conn.cursor()
