1fe69373b719bf96a9aa4243186419bbc52f5d21
artv15 <artv15@users.noreply.github.com>
2020-10-21 14:58:14 +0300

    setup trobleshoot

--- cogs/fsmoke.py
+++ cogs/fsmoke.py
@@ -12,40 +12,40 @@ import validators
 from discord.ext.commands.cooldowns import BucketType
 from time import gmtime, strftime
 
-class cs(commands.Cog):
+class fsmoke(commands.Cog):
     def __init__(self, bot):
-        self.Bot = cs
+        self.Bot = Bot
 
-@commands.group
-async def cs(self, ctx):
-	pass
+	@commands.group
+	async def cs(self, ctx):
+		pass
 
-@commands.command(pass_context=True)
-async def flash(self, ctx, bombsite: str):
-	bombsite = lower().bombsite
-	if bombsite == a or bombsite == b:
-		emb = discord.Embed(title=f"ФЛЕШКА НА " + bombsite, description="Бегите нахуй", color=0x4d0000)
-		log_message = await ctx.send(embed=emb)
-		print("Someone issued `flash` command. Message id: " + str(log_message))
-	else:
-		emb = discord.Embed(title=f"блять", description="где такой плент то, а?", color=0x4d0000)
+	@commands.command(pass_context=True)
+	async def flash(self, ctx, bombsite: str):
+		bombsite = lower().bombsite
+		if bombsite == a or bombsite == b:
+			emb = discord.Embed(title=f"ФЛЕШКА НА " + bombsite, description="Бегите нахуй", color=0x4d0000)
+			log_message = await ctx.send(embed=emb)
+			print("Someone issued `flash` command. Message id: " + str(log_message))
+		else:
+			emb = discord.Embed(title=f"блять", description="где такой плент то, а?", color=0x4d0000)
 
-@commands.command(pass_context=True)
-async def smoke(self, ctx, bombsite: str):
-	bombsite = lower().bombsite
-	if bombsite == a or bombsite == b:
-		emb = discord.Embed(title=f"СМОУК НА " + bombsite, description="Бегите нахуй", color=0x4d0000)
-		log_message = await ctx.send(embed=emb)
-		print("Someone issued `smoke` command. Message id: " + str(log_message))
-	else:
-		emb = discord.Embed(title=f"блять", description="где такой плент то, а?", color=0x4d0000)
+	@commands.command(pass_context=True)
+	async def smoke(self, ctx, bombsite: str):
+		bombsite = lower().bombsite
+		if bombsite == a or bombsite == b:
+			emb = discord.Embed(title=f"СМОУК НА " + bombsite, description="Бегите нахуй", color=0x4d0000)
+			log_message = await ctx.send(embed=emb)
+			print("Someone issued `smoke` command. Message id: " + str(log_message))
+		else:
+			emb = discord.Embed(title=f"блять", description="где такой плент то, а?", color=0x4d0000)
 
-@commands.command(pass_context=True)
-async def module(ctx):
-	emb=discord.Embed(title="Автор модуля flash", description="Treeshold#0218", color=0x4d0000)
-	emb.add_field(name="Автор идеи:", value="Dlorka#9909", inline=True)
-	await ctx.send(embed=emb)
+	@commands.command(pass_context=True)
+	async def module(ctx):
+		emb=discord.Embed(title="Автор модуля flash", description="Treeshold#0218", color=0x4d0000)
+		emb.add_field(name="Автор идеи:", value="Dlorka#9909", inline=True)
+		await ctx.send(embed=emb)
 
-class cs(commands.Cog):
-	def setup(bot):
-		bot.add_cog(cs(cs))
+
+def setup(Bot):
+	bot.add_cog(fsmoke(Bot))
